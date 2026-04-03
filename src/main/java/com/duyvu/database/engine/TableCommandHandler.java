87c3ed804dabc1480688aaa2a3396e763a74d004
duyvk <imkhuongduy@gmail.com>
2026-04-03 17:45:00 +0700

    format

--- src/main/java/com/duyvu/database/engine/TableCommandHandler.java
+++ src/main/java/com/duyvu/database/engine/TableCommandHandler.java
@@ -20,52 +20,52 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 
 class TableCommandHandler {
-	private Path getTablePath(String tableName) {
-		return Paths.get(EnvironmentUtils.getDatabasePath(), tableName + ".bin");
-	}
+  private Path getTablePath(String tableName) {
+    return Paths.get(EnvironmentUtils.getDatabasePath(), tableName + ".bin");
+  }
 
-	@SneakyThrows
-	void saveTableToFile(Table table) {
-		FileHandler.getInstance().addFileHandler(table.getPath());
-		RandomAccessFile raf = FileHandler.getInstance().getFileHandler(table.getPath());
+  @SneakyThrows
+  void saveTableToFile(Table table) {
+    FileHandler.getInstance().addFileHandler(table.getPath());
+    RandomAccessFile raf = FileHandler.getInstance().getFileHandler(table.getPath());
 
-		raf.seek(0);
-		TypeLengthValueReader converter = new TypeLengthValueReader();
-		byte[] data = converter.read(table.getHeader());
-		raf.write(data);
-	}
+    raf.seek(0);
+    TypeLengthValueReader converter = new TypeLengthValueReader();
+    byte[] data = converter.read(table.getHeader());
+    raf.write(data);
+  }
 
-	Table createTable(CreateTableCommand createTableCommand) {
-		Path tablePath = getTablePath(createTableCommand.getName());
+  Table createTable(CreateTableCommand createTableCommand) {
+    Path tablePath = getTablePath(createTableCommand.getName());
 
-		if (Files.exists(tablePath)) {
-			throw new DatabaseException(ErrorCode.TABLE_NAME_ALREADY_EXIST);
-		}
-		try {
-			PathUtils.createFileIfNotExists(tablePath);
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to create table file", e);
-		}
-		Table table = new Table(createTableCommand.getHeader(), tablePath);
+    if (Files.exists(tablePath)) {
+      throw new DatabaseException(ErrorCode.TABLE_NAME_ALREADY_EXIST);
+    }
+    try {
+      PathUtils.createFileIfNotExists(tablePath);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to create table file", e);
+    }
+    Table table = new Table(createTableCommand.getHeader(), tablePath);
 
-		saveTableToFile(table);
-		return table;
-	}
+    saveTableToFile(table);
+    return table;
+  }
 
-	Table getTable(String tableName) {
-		Path tablePath = getTablePath(tableName);
-		RandomAccessFile raf = FileHandler.getInstance().getFileHandler(tablePath);
+  Table getTable(String tableName) {
+    Path tablePath = getTablePath(tableName);
+    RandomAccessFile raf = FileHandler.getInstance().getFileHandler(tablePath);
 
-		HeaderReader headerReader = new HeaderReader();
-		return new Table(headerReader.read(raf), tablePath);
-	}
+    HeaderReader headerReader = new HeaderReader();
+    return new Table(headerReader.read(raf), tablePath);
+  }
 
-	void insert(InsertCommand insertCommand) {
-		Table table = getTable(insertCommand.getTableName());
-		Set<String> insertColumnNames = insertCommand.getValues().keySet();
-		Set<String> columnNames =
-				table.getHeader().columnDefinitions().stream()
-						.map(cd -> cd.columnName().getName())
-						.collect(toSet());
-	}
+  void insert(InsertCommand insertCommand) {
+    Table table = getTable(insertCommand.getTableName());
+    Set<String> insertColumnNames = insertCommand.getValues().keySet();
+    Set<String> columnNames =
+        table.getHeader().columnDefinitions().stream()
+            .map(cd -> cd.columnName().getName())
+            .collect(toSet());
+  }
 }
